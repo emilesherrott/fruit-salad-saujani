533675a500c83ed0418e3fcf19baccb2e495505c
Emile Sherrott Admin <emilesherrottadmin@Emiles-MacBook-Pro-2.local>
2026-04-23 15:58:39 +0100

    Add async branch

--- script.js
+++ script.js
@@ -13,22 +13,13 @@ function extractFruit(e) {
     e.target[0].value = ""
 }
 
-function fetchFruitData(fruit){
-    fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
-        .then(processResponse)
-        .then((data)=> addFruit(data))
-        .catch((error)=> console.log(error))
-    fetch(`https://pixabay.com/api/?key=<api-key>&q=${fruit}+fruit&image_type=photo&category=food`)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.log(error))
-}
-
-function processResponse(response){
-    if (response.status === 200){
-        return response.json()
-    } else {
-        throw "Error: https status code " + response.status
+async function fetchFruitData(fruit){
+    try {
+        const response = await fetch(`https://fruit-api-5v0j.onrender.com/fruits/${fruit}`)
+        const data = await response.json()
+        addFruit(data)
+    } catch (error) {
+        console.log(error)
     }
 }
 
